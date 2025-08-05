203120918f619bad1126f77f0fc3d2175de29d93
ArthurBerman <arthurberman94@gmail.com>
2025-08-05 15:45:28 -0400

    Add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import Bingo from "./Bingo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Bingo />
